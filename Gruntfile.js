b81f5ef851a0190912e344f80fbf3da8309d5813
Pedro Correia <pedro.a.correia@co.sapo.pt>
2014-05-26 14:00:07 +0100

    Adds gruntfile and font conversion and sass compilation tasks. Adds SASS code.

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    ii: grunt.file.readJSON('inkicons.json'),
+
+    clean: {
+      fonts: [
+        "<%= ii.dist.fonts %>*.eot",
+        "<%= ii.dist.fonts %>*.ttf",
+        "<%= ii.dist.fonts %>*.woff",
+      ],
+      css: [
+        '<%= ii.dist.css %>*.css'
+      ]
+    },
+
+    command : {
+        eot: {
+            cmd  : '<%= ii.commands.eot %> < <%= ii.src.ttf %>Ink-Icons.ttf > <%= ii.dist.fonts %>InkIcons.eot'
+        },
+        ttf: {
+            cmd  : 'cp <%= ii.src.ttf %>Ink-Icons.ttf <%= ii.dist.fonts %>InkIcons.ttf'
+        },
+        woff: {
+            cwd: './',
+            cmd  : '<%= ii.commands.woff %> <%= ii.dist.fonts %>InkIcons.ttf'
+        },
+    },
+
+    sass: {
+      dist: {
+        files: {
+          '<%= ii.dist.css %>ink-icons.css': '<%= ii.src.sass %>Ink-Icons.scss'
+        }
+      }
+    }
+
+
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-commands');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('default', ['webfonts','css']);
+  grunt.registerTask('webfonts', ['clean:fonts','command:eot','command:ttf','command:woff']);
+  grunt.registerTask('css', ['clean:css','sass']);
+
+};
---
+++ TODO.md
@@ -0,0 +1,8 @@
+# Icons
+* Check which font-awesome icone are currently being used in Ink and give priority to those
+
+# Font
+* Define baseline font settings (x-height, ascender, descender, caps)
+
+# Hinting
+* Hint for 14, 16, 18, 24, 32, 48 ppm
